6c485238b316a52048ef82e48f752b90c4901578
patrycjakwiecien <patrycjaapril@gmail.com>
2017-09-15 22:00:36 +0200

    ajax4

--- js.js
+++ js.js
@@ -2,7 +2,7 @@ function button() {
     $.getJSON('http://echo.jsontest.com/userId/108/userName/Akademia108/userURL/akademia108.pl',
         function (data) {
 
-            console.log(data);
+            console.log(data) ;
         });
 }
 
