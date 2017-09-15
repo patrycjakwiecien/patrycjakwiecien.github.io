003e9cf0526245f5cbda44626d6f1eae8480aa02
patrycjakwiecien <patrycjaapril@gmail.com>
2017-09-15 21:45:53 +0200

    ajax

---
+++ zadania-domowe/ajax_pobierz_dane/js/js.js
@@ -0,0 +1,27 @@
+function button() {
+    $.getJSON('http://echo.jsontest.com/userId/108/userName/Akademia108/userURL/akademia108.pl',
+        function (data) {
+
+            console.log(data);
+        });
+}
+
+$(document).ready(function () {
+    $("button").click(function () {
+        $.getJSON("http://echo.jsontest.com/userId/108/userName/Akademia108/userURL/akademia108.pl",
+
+            function (data) {
+                console.log(data);
+
+                $.each(data, function (e, i) {
+                    $("div").append("<br>" + "<br>" + e + ': ' + i);
+                });
+
+            });
+    });
+});
+
+
+
+
+
