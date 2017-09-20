0020fe3d0b29d441545f1fce0073bcb9ba67fc4d
patrycjakwiecien <patrycjaapril@gmail.com>
2017-09-20 19:21:38 +0200

    link

--- zadania-domowe/ajax_pobierz_dane/js/js.js
+++ zadania-domowe/ajax_pobierz_dane/js/js.js
@@ -1,5 +1,5 @@
 function button() {
-    $.getJSON('http://echo.jsontest.com/userId/108/userName/Akademia108/userURL/akademia108.pl',
+    $.getJSON("https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php",
         function (data) {
 
             console.log(data);
@@ -8,7 +8,7 @@ function button() {
 
 $(document).ready(function () {
     $("button").click(function () {
-        $.getJSON("http://echo.jsontest.com/userId/108/userName/Akademia108/userURL/akademia108.pl",
+        $.getJSON("https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php",
 
             function (data) {
                 console.log(data);
