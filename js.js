91faa1d84047d2a7dd5616120d2f2be39b550e7e
patrycjakwiecien <patrycjaapril@gmail.com>
2017-10-05 13:10:13 +0200

    zmiany

--- js.js
+++ js.js
@@ -21,6 +21,16 @@ $(document).ready(function () {
     });
 });
 
+$('.menu li a').click(function(e) {
+  var $this = $(this);
+  if (!$this.hasClass('active')) {
+    $this.addClass('active');
+  }
+  e.preventDefault();
+});
+
+
+
 
 
 
